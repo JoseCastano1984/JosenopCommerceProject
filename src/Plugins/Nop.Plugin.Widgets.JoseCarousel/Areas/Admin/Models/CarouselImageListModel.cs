8e9e3e1a45ff7e4de133929a86d3ff5621fe08da
JoseCastano1984 <jose.castano@sofasandstuff.com>
2025-03-03 15:34:16 +0000

    starting all over again

--- src/Plugins/Nop.Plugin.Widgets.JoseCarousel/Areas/Admin/Models/CarouselImageListModel.cs
+++
@@ -1,8 +0,0 @@
-using Nop.Web.Framework.Models;
-
-namespace Nop.Plugin.Widgets.JoseCarousel.Areas.Admin.Models;
-
-public record CarouselImageListModel : BasePagedListModel<CarouselImageModel>
-{
-    
-}
