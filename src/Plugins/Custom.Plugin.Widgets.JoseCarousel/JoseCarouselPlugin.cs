3dab4cef75978e92f0e2e842b89b36341ea306b3
JoseCastano1984 <jose.castano@sofasandstuff.com>
2025-03-04 12:26:47 +0000

    baseplugin class cleared

--- src/Plugins/Custom.Plugin.Widgets.JoseCarousel/JoseCarouselPlugin.cs
+++ src/Plugins/Custom.Plugin.Widgets.JoseCarousel/JoseCarouselPlugin.cs
@@ -5,17 +5,5 @@ namespace Custom.Plugin.Widgets.JoseCarousel;
 
 public class JoseCarouselPlugin : BasePlugin
 {
-    public override async Task InstallAsync()
-    {
-        //Logic during installation goes here...
-
-        await base.InstallAsync();
-    }
-
-    public override async Task UninstallAsync()
-    {
-        //Logic during uninstallation goes here...
-
-        await base.UninstallAsync();
-    }
+    
 }
