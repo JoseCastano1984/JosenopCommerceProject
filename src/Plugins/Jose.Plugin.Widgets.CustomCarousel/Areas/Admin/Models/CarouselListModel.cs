9f059cf8f747b9de9ceba127e0ee751a7db5d6f1
JoseCastano1984 <jose.castano@sofasandstuff.com>
2025-03-21 11:41:39 +0000

    deleting whole plugin

--- src/Plugins/Jose.Plugin.Widgets.CustomCarousel/Areas/Admin/Models/CarouselListModel.cs
+++
@@ -1,8 +0,0 @@
-using Nop.Web.Framework.Models;
-
-namespace Jose.Plugin.Widgets.CustomCarousel.Areas.Admin.Models;
-
-public record CarouselListModel : BasePagedListModel<CarouselModel>
-{
-    
-}
